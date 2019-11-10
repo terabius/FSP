56eebc255efbc65ec3c9b81be532b6585b3d904a
Bob Andre <bppa1905@gmail.com>
2019-11-10 13:40:48 -0800

    moved routed in root.jsx

--- app/javascript/components/Root.jsx
+++ app/javascript/components/Root.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import {Provider} from 'react-redux'
+import {BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
 export default function Root({store}) {
     return (
-        <div>
+        
             <Provider store={store} >
-                <App />
+                <Router>
+                    <App />
+                </Router>
             </Provider >            
-        </div>
+        
         )
     }
     
