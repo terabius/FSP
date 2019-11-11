29cc967bf3e51c635bfb43457ea6e85436ec67dc
Bob Andre <bppa1905@gmail.com>
2019-11-11 02:16:09 -0800

    price design

--- app/javascript/components/price/Price.jsx
+++ app/javascript/components/price/Price.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import PriceItem from './PriceItem'
 
 export default function Price() {
     return (
         <>
-            <div className="search-bar"></div>
+            <div className="search-bar">
+                <input className='input-search' type="text" placeholder='Search' />
+            </div>
 
-            <div className="crypto-tableau"></div>
+            <div className="price-tableau">
+            <div className="price-header">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div> 
+            </div>
+
+                <PriceItem />
+            </div>
         </>
     )
 }
 
 
+  
---
+++ app/javascript/components/price/PriceItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function PriceItem() {
+    return (
+        <>
+            <div className="price-item">
+                <div>number</div>
+                <div>name</div>
+                <div>price</div>
+                <div>change</div>
+                <div>Market cap</div>
+                <div>trade btn</div>
+                <div>follow star</div> 
+            </div>
+        </>
+    )
+}
