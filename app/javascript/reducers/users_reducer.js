0796e54caaae34f38de97912d126aed87948c4a5
Bob Andre <bppa1905@gmail.com>
2019-11-11 04:02:16 -0800

    clean

--- app/javascript/reducers/users_reducer.js
+++ app/javascript/reducers/users_reducer.js
@@ -1,7 +1,7 @@
 import {RECEIVE_CURRENT_USER} from '../actions/session_actions'
-import {LOGOUT_CURRENT_USER} from '../actions/session_actions'
+// import {LOGOUT_CURRENT_USER} from '../actions/session_actions'
 
-
+// 
 const usersReducer = (oldState ={}, action) => {
     Object.freeze(oldState);
 
@@ -9,8 +9,8 @@ const usersReducer = (oldState ={}, action) => {
         case RECEIVE_CURRENT_USER:
             return Object.assign({},oldState,{[action.currentUser.id]:action.currentUser});
 
-        case LOGOUT_CURRENT_USER:
-            return Object.assign({},oldState, {user:null} )
+        // case LOGOUT_CURRENT_USER:
+            // return Object.assign({},oldState, {user:null} )
         default:
             return oldState;
     }
