dae1865a6becc3348d81fc788d5e9f9a284271d9
Bob Andre <bppa1905@gmail.com>
2019-11-10 17:39:44 -0800

    route for home

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,18 @@
 import React from "react"
 import { Switch, Route} from "react-router-dom"
+
 import Home from './Home'
 import Navbar from './navbar'
-import Registration from './auth/Registration'
-import Login from './auth/Login'
 
 export default function App() {
   return (
     <React.Fragment>
 
-    <Navbar />
-    <Switch>
-    
-    
-    </Switch>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+
+      </Switch>
     </React.Fragment>          
   )
 }
--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,39 +1,12 @@
 import React, { Component } from 'react'
-import Registration from './auth/Registration'
-import Login from "./auth/Login"
-import Navbar from "./navbar"
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
 import HookHome from './HookHome'
 
 export default class Home extends Component {
     
-    constructor(props){
-        super(props);
-        
-        this.handleSuccess = this.handleSuccess.bind(this);
-    }
-    
-    handleLogoutClick(){
-        $.ajax({
-            method: "DELETE",
-            url: `/v1/session`
-        }).then(() => this.props.handleLogout())
-        
-    }
-    handleSuccess(data){
-        this.props.handleLogin(data);
-        this.props.history.push('/dashboard');
-    }
     render() {
         return (
             <React.Fragment>
-
-            <HookHome />
-            {/* <Navbar />
-            <HookHome />
-             */}
-            {/* <button onClick={()=>this.handleLogoutClick()}>Log out</button> */}
-
+                <HookHome />
             </React.Fragment>
             )
         }
