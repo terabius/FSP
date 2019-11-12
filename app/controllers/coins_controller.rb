71d6bd36f6c4162eb5a9e1c83343480fa39d225b
Bob Andre <bppa1905@gmail.com>
2019-11-11 20:27:03 -0800

    added create and destroy coin

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -6,14 +6,29 @@ class CoinsController < ApplicationController
   end
 
   def create
-  end
+    new_coin = Coin.new(coin_params)
+    new_coin.watchlist_id = current_user.watchlist.id
 
-  def update
-  end
-
-  def edit
+    if new_coin.save
+      render json: {status:200}
+    else
+      render json: {status:-1}
+    end
   end
 
   def destroy
+    coin_to_kill = current_user.watchlist.coins.find(:id)
+    if coin_to_kill.destroy
+      render json:{status:200}
+    else
+      render json:{status:-1}
+    end
   end
+
+
+  private 
+  def coin_params
+    params.require(:coin).permit(:id,:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   namespace :v1, defaults: {format: 'json'} do
-    resources :coins
+
+    resources :coins, only:[:create,:destroy]
     resources :watchlists, only: [:index]
     resources :users, only: [:create,:show,:edit,:update]
     resource :session, only:[:create,:destroy]
