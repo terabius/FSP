f87db55a103d9edf2dea02c243ae5b41782c800c
Bob Andre <bppa1905@gmail.com>
2019-12-11 03:06:27 -0800

    rest

--- app/javascript/components/PanelItem.jsx
+++ app/javascript/components/PanelItem.jsx
@@ -10,12 +10,12 @@ export default class PanelItem extends Component {
             <>
                 <div>{this.props.num} </div>
                 <div className="panel-name"> {this.props.name}</div>
-                <div className='panel-price'>{this.props.price}</div>
-                <div className='panel-change'>{this.props.change}</div>
+                <div className='panel-price'>${this.props.price}</div>
+                <div className='panel-change'>{this.props.change}%</div>
                 <div className='panel-chart'>{this.props.chart}</div>
-                <div className='panel-action'>
-                    <div className='btn btn-flat'>Trade</div>
-                </div>
+                
+                    <div className='btn btn-flat green'>Buy</div>
+                
                 <div className="panel-dummy"></div>
             </>
         )
