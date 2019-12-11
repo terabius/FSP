07968bc6d7bdb37e5d06cd1e8ecce4edfdb1e81a
Bob Andre <bppa1905@gmail.com>
2019-12-10 21:50:44 -0800

    css

--- app/javascript/components/Panel.jsx
+++ app/javascript/components/Panel.jsx
@@ -5,21 +5,22 @@ export default function Panel() {
     return (
         <>
         
-        <div className="panel-tableau">
-            <div className="panel-tableau-kid">
-                <div className='panel-item'># <span className='panel-name'>Name</span></div>
+        <div className="container-tableau">
+            <div className="row-tableau">
+                <div className='panel-item'># </div>
+                <div className='panel-name'>Name</div>
                 <div className='panel-item'>Price</div>
                 <div className='panel-item'>Change</div>
                 <div className='panel-item'>Chart</div>
                 <div className='panel-item'>Trade</div>
             </div>
             
-                    {[{ name: 'Bitcoin', price: 300, change: 2, chart: 4 },
-                    { name: 'Ethereum', price: 300, change: 3, chart: 4 },
-                    { name: 'Bitcoin Cash', price: 300, change: 3, chart: 4 },
-                    { name: 'Litecoin', price: 300, change: 3, chart: 4 }].map((data,idx)=>
+            {[{ name: 'Bitcoin', price: 300, change: 2, chart: 4 },
+            { name: 'Ethereum', price: 300, change: 3, chart: 4 },
+            { name: 'Bitcoin Cash', price: 300, change: 3, chart: 4 },
+            { name: 'Litecoin', price: 300, change: 3, chart: 4 }].map((data,idx)=>
 
-                    <div className="panel-tableau-kid" key={data.name}>
+                <div className="row-tableau" key={data.name}>
 
                     <PanelItem name={data.name} 
                     num={idx}
@@ -28,8 +29,8 @@ export default function Panel() {
                     chart={data.chart}
                     />
 
-                    </div>
-                )}
+                </div>
+            )}
                     
         </div>
 
