acac0746b8bd0314fb0d54d218ba9313affea6ab
Bob Andre <bppa1905@gmail.com>
2019-12-16 23:36:57 -0800

    cmp four done

--- app/javascript/components/home/ContentHomeFour.jsx
+++ app/javascript/components/home/ContentHomeFour.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 export default function ContentHomeFour() {
     return (
         <div className='contentFour-container'>
-            <div className="contentFour-data">$$$$$$</div>
-            <div className="contentFour-data">130M</div>
-            <div className="contentFour-data">260B</div>
+            <div className="contentFour-data">$150B+ <br/> <span>Cryptocurrency exchanged</span></div>
+            <div className="contentFour-data">102 <br /> <span>Countries supported</span></div>
+            <div className="contentFour-data">30M+ <br /> <span>Customers served</span></div>
         </div>
     )
 }
+
