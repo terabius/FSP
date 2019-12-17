e5a7e401290782a4508033597387a039b48b1e2d
Bob Andre <bppa1905@gmail.com>
2019-12-16 23:47:29 -0800

    cmp done v1

--- app/javascript/components/home/FooterHome.jsx
+++ app/javascript/components/home/FooterHome.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function FooterHome() {
     return (
         <div className='footerHome-container'>
-            <div className="footerHome-data">LOGOs</div>
+            <div className="footerHome-data">
+                <span className="footertitle">Koinbase</span><br/>
+                <br/>
+                <p>###-###-#### <br/><br/>
+                   easynametoremember.herokuapp.com
+                </p><br/>
+                <p>© 2019 Koinbase</p>
+            </div>
             <div className="footerHome-data">product</div>
             <div className="footerHome-data">learn</div>
             <div className="footerHome-data">company</div>
