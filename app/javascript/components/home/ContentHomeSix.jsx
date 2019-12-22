8de160fa686376521e391c9b9394a1adf1e294ed
Bob Andre <bppa1905@gmail.com>
2019-12-22 03:43:49 -0800

    home missing stuff done

--- app/javascript/components/home/ContentHomeSix.jsx
+++ app/javascript/components/home/ContentHomeSix.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import banner from '../../../assets/images/banner.jpg'
+
 export default function ContentHomeSix() {
     return (
-        <div>
-            
+        <div className='contentSix-container'>
+            <div>
+                <div className="contentOne-col">
+                    <h2>
+                        Earn up to US$126 <br />
+                        worth of crypto
+                    </h2>
+                    <p>
+                        Discover how specific cryptocurrencies work — and<br />
+                        get a bit of each crypto to try out for yourself.
+                    </p> <br />
+                    <div><a href="" className="btn btn-flat">Start earning</a></div>
+                </div>
+            </div>
+            <div>
+                <img src={banner} alt="banner"/>
+            </div>
         </div>
     )
 }
